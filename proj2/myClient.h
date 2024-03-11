a4744f72cf450a2f99a3d06682e0430eece4ed6a
raej15 <sej15@email.sc.edu>
2024-03-11 18:06:44 -0400

    hh

--- proj2/myClient.h
+++ proj2/myClient.h
@@ -8,12 +8,14 @@
 #include <vector>
 
 
-float add(float a, float b);
-float subtract(float a, float b);
-float multiply(float a, float b);
-float divide(float a, float b);
-std::vector<std::string> addSub(std::vector<std::string> eqn);
-std::vector<std::string> multDiv(std::vector<std::string> eqn);
-void run(std::vector<std::string>);
+// float add(float a, float b);
+// float subtract(float a, float b);
+// float multiply(float a, float b);
+// float divide(float a, float b);
+// std::vector<std::string> addSub(std::vector<std::string> eqn);
+// std::vector<std::string> multDiv(std::vector<std::string> eqn);
+// void run(std::vector<std::string>);
+
+
 
 #endif  // PROJ2_MYCLIENT_H_
