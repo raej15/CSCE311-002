601132847108de6c9c9698e1e26559b5ab7032a8
raej15 <sej15@email.sc.edu>
2024-03-12 21:48:02 -0400

    headers cpplint

--- proj2/myClient.h
+++ proj2/myClient.h
@@ -1,3 +1,4 @@
+//  COPYRIGHT 2024 Rae Jones
 #ifndef PROJ2_MYCLIENT_H_
 #define PROJ2_MYCLIENT_H_
 #define SOCKET_NAME "/tmp/9Lq7BNBnBycd6nxy.socket"
@@ -7,14 +8,13 @@
 #include <iostream>
 #include <vector>
 
-
-// float add(float a, float b);
-// float subtract(float a, float b);
-// float multiply(float a, float b);
-// float divide(float a, float b);
-// std::vector<std::string> addSub(std::vector<std::string> eqn);
-// std::vector<std::string> multDiv(std::vector<std::string> eqn);
-// void run(std::vector<std::string>);
+float add(float a, float b);
+float subtract(float a, float b);
+float multiply(float a, float b);
+float divide(float a, float b);
+std::vector<std::string> addSub(std::vector<std::string> eqn);
+std::vector<std::string> multDiv(std::vector<std::string> eqn);
+void run(std::vector<std::string>);
 
 
 
--- proj2/myServer.h
+++ proj2/myServer.h
@@ -1,11 +1,19 @@
+//  COPYRIGHT 2024 Rae Jones
 #ifndef PROJ2_MYSERVER_H_
 #define PROJ2_MYSERVER_H_
-#define SOCKET_NAME "/tmp/9Lq7BNBnBycd6nxy.socket" // get rid of this
-#define BUFFER_SIZE 5000 // get rid of this
+#define SOCKET_NAME "/tmp/9Lq7BNBnBycd6nxy.socket"  // get rid of this
+#define BUFFER_SIZE 5000  // get rid of this
 
 #include <iostream>
 #include <string>
 #include <vector>
 
+float add(float a, float b);
+float subtract(float a, float b);
+float multiply(float a, float b);
+float divide(float a, float b);
+std::vector<std::string> addSub(std::vector<std::string> eqn);
+std::vector<std::string> multDiv(std::vector<std::string> eqn);
+void run(std::vector<std::string>);
 
 #endif  // PROJ2_MYSERVER_H_
