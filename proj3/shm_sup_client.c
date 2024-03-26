ddaade465867b69dbb85bdfa9c516a0fb8fc5e8a
raej15 <sej15@email.sc.edu>
2024-03-26 00:55:04 -0400

    addded threads

--- proj3/shm_sup_client.c
+++ proj3/shm_sup_client.c
@@ -1,7 +1,19 @@
 // Copyright 2023 LittleCube
 //
 
-#include </acct/sej15/Desktop/CSCE311-002/proj3/shm_sup.h> //change
+#include <proj3/shm_sup.h> //change
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+void *print_message_function( void *ptr );
+
+void *print_message_function( void *ptr )
+{
+     char *message;
+     message = (char *) ptr;
+     printf("%s \n", message);
+}
+
 struct shmbuf* shmp;
 
 int main(int argc, char** argv) {
@@ -67,4 +79,25 @@ int main(int argc, char** argv) {
 
     // notify server that string is ready to read
     sem_post(sem2);
+
+     pthread_t thread1, thread2;
+     char *message1 = "Thread 1";
+     char *message2 = "Thread 2";
+     int  iret1, iret2;
+
+    /* Create independent threads each of which will execute function */
+
+     iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
+     iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);
+
+     /* Wait till threads are complete before main continues. Unless we  */
+     /* wait we run the risk of executing an exit which will terminate   */
+     /* the process and all threads before the threads have completed.   */
+
+     pthread_join( thread1, NULL);
+     pthread_join( thread2, NULL); 
+
+     printf("Thread 1 returns: %d\n",iret1);
+     printf("Thread 2 returns: %d\n",iret2);
+     exit(0);
 }
