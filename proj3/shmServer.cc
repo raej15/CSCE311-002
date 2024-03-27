7f6cbbec92e32cdc0b2344fd02f4800892b72b41
raej15 <sej15@email.sc.edu>
2024-03-27 15:27:34 -0400

    erm

--- proj3/shmServer.cc
+++ proj3/shmServer.cc
@@ -15,46 +15,58 @@
 #include <sstream>
 #include <signal.h>
 
-struct shmbuf* shmp;
+struct shmbuf *shmp;
 
-float add(float a, float b) {
+float add(float a, float b)
+{
     return a + b;
 }
-float subtract(float a, float b) {
+float subtract(float a, float b)
+{
     return a - b;
 }
-float multiply(float a, float b) {
-    return a*b;
+float multiply(float a, float b)
+{
+    return a * b;
 }
 
-float divide(float a, float b) {
-    return a/b;
+float divide(float a, float b)
+{
+    return a / b;
 }
 
 //  finds and performs all the addition and subtraction operations in vector
-std::vector<std::string> addSub(std::vector<std::string> eqn) {
+std::vector<std::string> addSub(std::vector<std::string> eqn)
+{
     std::vector<std::string> result = eqn;
     auto itr = result.begin();
-    while ( itr != result.end() ) {
-        if (*itr == "+" || *itr == "-") {
+    while (itr != result.end())
+    {
+        if (*itr == "+" || *itr == "-")
+        {
             //  get the two surrounding floats
-            float a = std::stof(*(itr-1));
-            float b = std::stof(*(itr+1));
+            float a = std::stof(*(itr - 1));
+            float b = std::stof(*(itr + 1));
 
-            if (*itr == "+") {
+            if (*itr == "+")
+            {
                 //  perform addition, replace "+" with result
                 *itr = std::to_string(add(a, b));
                 std::cout << a << "+" << b << subtract(a, b);
-            } else {
+            }
+            else
+            {
                 //  perform subtraction, replace "-" with result
                 *itr = std::to_string(subtract(a, b));
-               std::cout << a << "-" << b << subtract(a, b);
+                std::cout << a << "-" << b << subtract(a, b);
             }
 
             //  remove the two surrounding floats
-            itr = result.erase(itr-1);
-            itr = result.erase(itr+1);
-        } else {
+            itr = result.erase(itr - 1);
+            itr = result.erase(itr + 1);
+        }
+        else
+        {
             //  move to the next element
             ++itr;
         }
@@ -64,28 +76,35 @@ std::vector<std::string> addSub(std::vector<std::string> eqn) {
 }
 
 //  finds and performs all the addition and subtraction operations in vector
-std::vector<std::string> multDiv(std::vector<std::string> eqn) {
+std::vector<std::string> multDiv(std::vector<std::string> eqn)
+{
     std::vector<std::string> result = eqn;
     auto itr = result.begin();
-    while (itr != result.end()) {
-        if (*itr == "x" || *itr == "/") {
+    while (itr != result.end())
+    {
+        if (*itr == "x" || *itr == "/")
+        {
             //  get the two surrounding floats
-            float a = std::stof(*(itr-1));
-            float b = std::stof(*(itr+1));
+            float a = std::stof(*(itr - 1));
+            float b = std::stof(*(itr + 1));
 
-            if (*itr == "/") {
+            if (*itr == "/")
+            {
                 //  perform division, replace "/" with result
                 *itr = std::to_string(divide(a, b));
-            } else {
+            }
+            else
+            {
                 //  perform multiplication, replace "x" with result
                 *itr = std::to_string(multiply(a, b));
             }
 
             //  remove the two surrounding floats
-            itr = result.erase(itr-1);
-            itr = result.erase(itr+1);
-
-        } else {
+            itr = result.erase(itr - 1);
+            itr = result.erase(itr + 1);
+        }
+        else
+        {
             //  move to the next element
             ++itr;
         }
@@ -95,41 +114,49 @@ std::vector<std::string> multDiv(std::vector<std::string> eqn) {
     return result;
 }
 
-std::vector<std::string> loadData(std::string fileName) {
-    //std::cout << "entered filename: " << fileName << std::endl;
+std::vector<std::string> loadData(std::string fileName)
+{
+    // std::cout << "entered filename: " << fileName << std::endl;
     std::ifstream currFile2("dat/equations_691.txt"); // CHANGE!!!!
     std::ifstream currFile(fileName.c_str());
-    //std::ifstream currFile;
-    //currFile.open(fileName.c_str());
+    // std::ifstream currFile;
+    // currFile.open(fileName.c_str());
     std::vector<std::string> data;
     std::string line;
-    if (currFile.is_open()) {
-        while (getline(currFile, line)) {
-        data.push_back(line);
-        //std::cout << line << std::endl;
+    if (currFile.is_open())
+    {
+        while (getline(currFile, line))
+        {
+            data.push_back(line);
+            // std::cout << line << std::endl;
         }
         currFile.close();
-       std::cout << "\tFILE CLOSED" << std::endl;
-    } else {
+        std::cout << "\tFILE CLOSED" << std::endl;
+    }
+    else
+    {
         data.push_back("INVALID FILE");
-      return data;
+        return data;
     }
 
     return data;
 }
 
 void parseArgs(std::vector<std::string> data,
-    std::vector<std::string> argLines) {
+               std::vector<std::string> argLines)
+{
     int argSize = argLines.size();
-    for (int i = 0; i < argSize; i++) {
+    for (int i = 0; i < argSize; i++)
+    {
         std::vector<std::string> parsedEqn;
-        int curr = stoi(argLines.at(i))-1;
+        int curr = stoi(argLines.at(i)) - 1;
         std::string eqn = data.at(curr);
         std::cout << "EQUATION: " << eqn << std::endl;
 
         std::istringstream ss(eqn);
         std::string element;
-        while (ss >> element) {
+        while (ss >> element)
+        {
             std::cout << element << std::endl;
             parsedEqn.push_back(element);
         }
@@ -138,94 +165,111 @@ void parseArgs(std::vector<std::string> data,
     // return ;
 }
 
-std::string clientEqns(std::vector<std::string> data) {
+std::string clientEqns(std::vector<std::string> data)
+{
     std::string finalStrng = "";
-       int argSize = data.size();
-    for (int i=0; i < argSize; i++) {
+    int argSize = data.size();
+    for (int i = 0; i < argSize; i++)
+    {
         std::string eqn = data.at(i);
         finalStrng.append(eqn);
         finalStrng.append("\n");
     }
     return finalStrng;
 }
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     std::string path;
     std::vector<std::string> data;
-    std::cout<< "SERVER STARTED" << std::endl;
-    
-    // happy signal time (properly cleanup on terminate)
- //   signal(SIGTERM, quit);
-//    signal(SIGINT, quit);
 
-    // make sure semaphores do not already exist
+    // STEP 1: write SERVER STARTED to stdout
+    std::cout << "SERVER STARTED" << std::endl;
+
+    // unlinking any pre-existing semaphores
     sem_unlink(SEM_SERVER);
     sem_unlink(SEM_CLIENT);
-while (true) {
-    // create previously nonexistent semaphores
-    // sem_open returns -1 on fail, perhaps worth checking
-    sem_t *sem1 = sem_open(SEM_SERVER, O_CREAT, 0660, 0);
-    sem_t *sem2 = sem_open(SEM_CLIENT, O_CREAT, 0660, 0);
-    sem_t *sem3 = sem_open(SEM_SERVER, O_CREAT, 0660, 0);
+    while (true)
+    {
+        // create semaphores
+        sem_t *sem1 = sem_open(SEM_SERVER, O_CREAT, 0660, 0);
+        sem_t *sem2 = sem_open(SEM_CLIENT, O_CREAT, 0660, 0);
+        sem_t *sem3 = sem_open(SEM_SERVER, O_CREAT, 0660, 0);
+
+        // wait for client to open shared memory
+        sem_wait(sem2);
+        //std::cout << "CLIENT REQUEST RECIEVED" << std::endl;
+
+        // // so does shm_open
+        // int shmfd = shm_open(SHMPATH, O_RDWR, 0);
+        // std::cout << "\tMEMORY OPEN" << std::endl;
+
+        // // map shared memory
+        // // BUFFER_SIZE is defined in shm_sup.h
+        // shmp = (shmbuf *)mmap(0,
+        //                       sizeof(*shmp),
+        //                       PROT_READ | PROT_WRITE,
+        //                       MAP_SHARED,
+        //                       shmfd,
+        //                       0);
+
+        // char read_buffer[BUFFER_SIZE];
+
+        // ready to read from client
+        sem_post(sem1);
+
+        // wait for client to finish writing
+        sem_wait(sem2);
+
+        // read path from shared memory
+        snprintf(read_buffer, BUFFER_SIZE, "%s", shmp->buf);
+                std::cout << "CLIENT REQUEST RECIEVED" << std::endl;
+
+        // so does shm_open
+        int shmfd = shm_open(SHMPATH, O_RDWR, 0);
+        std::cout << "\tMEMORY OPEN" << std::endl;
+
+        // map shared memory
+        // BUFFER_SIZE is defined in shm_sup.h
+        shmp = (shmbuf *)mmap(0,
+                              sizeof(*shmp),
+                              PROT_READ | PROT_WRITE,
+                              MAP_SHARED,
+                              shmfd,
+                              0);
+
+        char read_buffer[BUFFER_SIZE];
 
 
-    // wait for client to open shared memory
-    sem_wait(sem2);
-    std::cout << "CLIENT REQUEST RECIEVED" << std::endl;
+        // print client path from read_buffer
+        std::cout << "\tOPENING: " << read_buffer << std::endl;
+        path = std::string(read_buffer);
+        data = loadData(path);
+        std::string eqnstr = clientEqns(data);
 
-    // so does shm_open
-    int shmfd = shm_open(SHMPATH, O_RDWR, 0);
-    std::cout << "\tMEMORY OPEN" << std::endl;
+        // ready to read from client
+        sem_post(sem1);
 
-    // map shared memory
-    // BUFFER_SIZE is defined in shm_sup.h
-    shmp = (shmbuf*)mmap(0,
-                sizeof(*shmp),
-                PROT_READ | PROT_WRITE,
-                MAP_SHARED,
-                shmfd,
-                0);
+        // wait for client to finish writing
+        sem_wait(sem2);
 
-    char read_buffer[BUFFER_SIZE];
+        // read string from shared memory
+        snprintf(read_buffer, BUFFER_SIZE, "%s", shmp->buf);
 
-    // ready to read from client
-    sem_post(sem1);
+        // print client string from read_buffer
+        // printf("LINES: %s (REMOVE)", read_buffer);
 
-    // wait for client to finish writing
-    sem_wait(sem2);
+        // ready to write file to client
+        sem_post(sem3);
 
-    // read path from shared memory
-    snprintf(read_buffer, BUFFER_SIZE, "%s", shmp->buf);
+        // writing file to client
+        snprintf(shmp->buf, BUFFER_SIZE, "%s\n", eqnstr.c_str());
 
-    // print client path from read_buffer
-    std::cout<< "\tOPENING: " << read_buffer <<std::endl;
-    path = std::string(read_buffer);
-    data = loadData(path);
-    std::string eqnstr = clientEqns(data);
-    
-    // ready to read from client
-    sem_post(sem1);
-
-    // wait for client to finish writing
-    sem_wait(sem2);
-
-    // read string from shared memory
-    snprintf(read_buffer, BUFFER_SIZE, "%s", shmp->buf);
-
-    // print client string from read_buffer
-   // printf("LINES: %s (REMOVE)", read_buffer);
-    
-    // ready to write file to client
-    sem_post(sem3);
-
-    // writing file to client
-    snprintf(shmp->buf, BUFFER_SIZE, "%s\n", eqnstr.c_str());
-
-    //printf("SERVER: GOODBYE\n");
-}
+        // printf("SERVER: GOODBYE\n");
+    }
 }
 
 // I'm a happy signal boy
-//void quit() {
+// void quit() {
 //    shm_unlink(SHMPATH);
 //    sem_unlink(SEM_SERVER);
 //    sem_unlink(SEM_CLIENT);
