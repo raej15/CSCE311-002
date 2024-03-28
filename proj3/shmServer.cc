23cd81ea753ad584d479e32e2eaf21cac16a5d80
raej15 <sej15@email.sc.edu>
2024-03-27 23:59:57 -0400

    more testng

--- proj3/shmServer.cc
+++ proj3/shmServer.cc
@@ -139,6 +139,8 @@ int main(int argc, char **argv) {
             else if (counter == 1)
             {
                 snprintf(store_->buf[1] + count, shared_mem_struct::kCols - count, "%s", curr.c_str());
+                std::cout << "0: " << store_->buf[0] << std::endl;
+
             }
             else if (counter == 2)
             {
