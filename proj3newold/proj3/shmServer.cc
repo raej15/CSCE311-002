7309e7efea51a0472b27cdff4b27fdf09cabefe9
raej15 <sej15@email.sc.edu>
2024-03-28 09:28:49 -0400

    fix printouts

--- proj3newold/proj3/shmServer.cc
+++ proj3newold/proj3/shmServer.cc
@@ -131,7 +131,7 @@ int main(int argc, char **argv) {
         // ready to write to client
         sem_post(sem1);
         snprintf(store_->buf[1], shared_mem_struct::kCols, "%s", eqnstr.substr(0, 12).c_str());
-        std::cout << "BUFFER CONTENTS" store_->buf[1] << std::endl;
+        std::cout << "BUFFER CONTENTS" << store_->buf[1] << std::endl;
 
 
         // writing file to client
