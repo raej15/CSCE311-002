1d99dcc49726b2b7d5a96ae1e89e890a46908f5f
raej15 <sej15@email.sc.edu>
2024-03-28 00:42:16 -0400

    d

--- proj3/shmServer.cc
+++ proj3/shmServer.cc
@@ -117,7 +117,7 @@ int main(int argc, char **argv) {
         // FILE CLOSED is handled within loadData function
         data = loadData(path);
         std::string eqnstr = clientEqns(data);
-        snprintf(store_->buf[0], shared_mem_struct::kCols, "%s", eqnstr.substr(0, 10).c_str());
+        snprintf(store_->buf[0], shared_mem_struct::kCols, "%s", eqnstr.substr(0, 11).c_str());
         std::cout << store_->buf[0] << std::endl;
         std::stringstream eqnstream(eqnstr);
 
